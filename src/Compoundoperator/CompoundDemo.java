530e057cc4e33d926e5aa07c6a86c94bd1cad5d5
Rahulstar12345 <rs7350245951@gmail.com>
2023-04-20 13:31:16 +0530

    this is done by Rahul first commit

---
+++ src/Compoundoperator/CompoundDemo.java
@@ -0,0 +1,12 @@
+package Compoundoperator;
+
+public class CompoundDemo {
+	public static void main(String[] args) {
+		int a=23;
+		System.out.println(a);
+		//a=a+a;
+		a+=a;
+		System.out.println(a);
+	}
+
+}
---
+++ src/SimpleAssignment/SimpleDemo.java
@@ -0,0 +1,9 @@
+package SimpleAssignment;
+
+public class SimpleDemo {
+	public static void main(String[] args) {
+		int a=30;
+		System.out.println(a);
+	}
+
+}
